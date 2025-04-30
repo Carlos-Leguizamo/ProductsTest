58b9a1a387ff2ecd8155b6e83615fb94427d89c3
Carlos Eduardo Leguizamo Ramirez <126182094+Carlos-Leguizamo@users.noreply.github.com>
2025-04-29 19:29:56 -0500

    href y marca de agua

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,16 +1,49 @@
 import { useInitProducts } from '../hooks/useInitProducts';
 import { ProductForm } from '../components/ProductForm';
 import { ProductList } from '../components/ProductList';
-import { Container, Typography } from '@mui/material';
+import { Container, Typography, Box, Link } from "@mui/material";
+import GitHubIcon from "@mui/icons-material/GitHub"; 
 
 const Home = () => {
   useInitProducts();
 
   return (
     <Container>
-      <Typography variant="h4" mt={4} mb={2} sx={{textAlign:'center'}}>Gestor de Productos</Typography>
+      <Typography variant="h4" mt={4} mb={2} sx={{ textAlign: "center" }}>
+        Gestor de Productos
+      </Typography>
       <ProductForm />
       <ProductList />
+      <Box sx={{ position: "relative", mt: 4 }}>
+        <Typography
+          variant="body2"
+          sx={{
+            position: "absolute",
+            bottom: 0,
+            right: 0,
+            opacity: 0.8,
+            fontSize: "0.85rem",
+            display: "flex",
+            alignItems: "center",
+            gap: "4px",
+          }}
+        >
+          Creado por{" "}
+          <Link
+            href="https://github.com/Carlos-Leguizamo"
+            target="_blank"
+            rel="noopener noreferrer"
+            sx={{
+              color: "#1976d2",
+              textDecoration: "none",
+              fontWeight: "bold",
+            }}
+          >
+            <GitHubIcon fontSize="small" />
+            Carlos Leguizamo
+          </Link>
+        </Typography>
+      </Box>
     </Container>
   );
 };
